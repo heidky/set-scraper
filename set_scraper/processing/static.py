bf2d078b23dc2ca00c793c28e448076e1af5f742
HeidKy <heid.ky8@gmail.com>
2024-02-18 23:37:13 +0100

    Added ws bkrr

--- set_scraper/processing/static.py
+++ set_scraper/processing/static.py
@@ -1,7 +1,8 @@
 from PIL import Image
 from math import floor
 
-def resize_to_screen(img: Image.Image, w_screen: int, h_screen: int):
+
+def resize_to_screen(img: Image.Image, w_screen: int, h_screen: int) -> Image.Image:
     w, h = img.size
     ratio = max(w / w_screen, h / h_screen)
 
@@ -10,3 +11,28 @@ def resize_to_screen(img: Image.Image, w_screen: int, h_screen: int):
     else:
         new_size = (floor(w/ratio), floor(h/ratio))
         return img.resize(new_size)
+    
+
+class Resizer:
+    def __init__(self, screen_size: str | tuple[int, int]='2K'):
+        if isinstance(screen_size, str):
+            screen_size = screen_size.lower()
+
+            if screen_size == '2k':
+                self.w_screen = 2560
+                self.h_screen = 1440
+            elif screen_size == '4k':
+                self.w_screen = 3840 
+                self.h_screen = 2160
+            elif screen_size == '1080p':
+                self.w_screen = 1920
+                self.h_screen = 1080
+            else:
+                raise ValueError("screen size as string must be one of: [1080p, 2K, 4K]")
+        else:
+            w, h = screen_size
+            self.w_screen = round(w)
+            self.h_screen = round(h)
+        
+    def process(self, image: Image.Image) -> Image.Image:
+        return resize_to_screen(image, self.w_screen, self.h_screen)
--- set_scraper/scrape.py
+++ set_scraper/scrape.py
@@ -5,68 +5,70 @@ import websites.vg as vg
 import websites.bunkr as bunkr
 import download.downloader as dw
 from pathlib import Path
+import processing.static as ps
 
 load_dotenv()
 sc_cookie = os.getenv("SC_COOKIE")
 
 
-def sc_scrape_post(set_name, url):
-    output_path = Path('./sets/original') / set_name
-
-    post = sc.Post_Sc(url, set_name, cookie=sc_cookie)
-    images = post.get_images()
-
-    downloader = dw.DownloaderSimple(images, output_path)
-    downloader.download()
-
-
-def vg_scrape_post(set_name, url):
-    output_path = Path('./sets/original') / set_name
-
-    post = vg.Post_Vg(url, set_name)
-    images = post.get_images()
-
-    downloader = dw.DownloaderSimple(images, output_path)
-    downloader.download()
-
-
 def scrape_post(set_name, url):
     output_path = Path('./sets/original') / set_name
+    post = None
 
-    post = bunkr.Post_Bunkr(url, set_name)
+    match url:
+        case _ if '://bunkr.' in url or '://bunkrr.' in url:
+            post = bunkr.Post_Bunkr(url, set_name)
+        case _ if '://vipergirls.' in url:
+            post = vg.Post_Vg(url, set_name)
+        case _ if '://simpcity.' in url:
+            post = sc.Post_Sc(url, set_name, cookie=sc_cookie)
+
+    if post is None:
+        raise RuntimeError("Not valid match for url:", url)
+    
     images = post.get_images()
 
-    downloader = dw.DownloaderSimple(images, output_path)
+    processing = [ps.Resizer('2K')]
+    downloader = dw.DownloaderSimple(images, output_path, processing=processing)
     downloader.download()
 
 
 if __name__ == '__main__':
-    # url = "https://vipergirls.to/threads/5423042-Emily-Bloom-Valley-x50-6720px-(07-21-20)?highlight=emily+bloom"
-    # url="https://vipergirls.to/threads/6872723-Jessica-Night-WHITE-amp-BLUE-ALL-OVER-CARD-f0993-x-50-3000-x-4500-March-16-2022?highlight=istripper"
-    # thread = vg.Thread_VG(url)
-    # thread.extract_sets()
-    # thread.solve_sets()
-
-    # downloader = dw.Downloader('./sets/original', 'Jni - White', thread.sets, img_sleep_secs=0.2)
-    # downloader.download()
-
-    # url = 'https://simpcity.su/threads/jessica-nigri.9946/'
-    # print('hello')
-    # url = 'https://simpcity.su/threads/jinx-asmr.18872/'
-    # thread = sc.Thread_SC(url, 'Jinx', sc_cookie)
-    # thread.extract_sets()
-    # thread.solve_sets()
-    # print(thread.sets)
-    
-    # downloader = dw.Downloader('./sets/original', 'Jinx', thread.sets, img_sleep_secs=0.1)
-    # downloader.download()
-
-    # sc_scrape_post("Jinx - Fireforce", "https://simpcity.su/threads/jinx-asmr.18872/post-79635")
-    # sc_scrape_post("Jinx - Hearts", "https://simpcity.su/threads/jinx-asmr.18872/post-3442609")
-    # sc_scrape_post("Octokuro - Vampire", "https://simpcity.su/threads/octokuro.9999/post-3435093")
-    # sc_scrape_post("Jessica Nigri - Deer Queen", "https://simpcity.su/threads/jessica-nigri.9946/post-3290110")
-    # vg_scrape_post("Emily - Valley", "https://vipergirls.to/threads/5423042-Emily-Bloom-Valley-x50-6720px-(07-21-20)?highlight=emily+bloom")
+    # scrape_post("Jinx - Fireforce", "https://simpcity.su/threads/jinx-asmr.18872/post-79635")
+    # scrape_post("Jinx - Hearts", "https://simpcity.su/threads/jinx-asmr.18872/post-3442609")
+    # scrape_post("Octokuro - Vampire", "https://simpcity.su/threads/octokuro.9999/post-3435093")
+    # scrape_post("Jessica Nigri - Deer Queen", "https://simpcity.su/threads/jessica-nigri.9946/post-3290110")
+    scrape_post("Emily - Valley", "https://vipergirls.to/threads/5423042-Emily-Bloom-Valley-x50-6720px-(07-21-20)?highlight=emily+bloom")
     # scrape_post("Jinx - Bunkr", "https://bunkr.si/a/tA3SsEfI")
     # scrape_post("Jinx - Bunkr 2", "https://bunkrr.su/a/xx7U7vrJ")
-    scrape_post("Caprice - Private Show", "https://bunkrr.su/a/9GtpvaJ3")
+    # scrape_post("Caprice - Private Show", "https://bunkrr.su/a/9GtpvaJ3")
+
+
+
+
+
+
+
+
+
+
+# url = "https://vipergirls.to/threads/5423042-Emily-Bloom-Valley-x50-6720px-(07-21-20)?highlight=emily+bloom"
+# url="https://vipergirls.to/threads/6872723-Jessica-Night-WHITE-amp-BLUE-ALL-OVER-CARD-f0993-x-50-3000-x-4500-March-16-2022?highlight=istripper"
+# thread = vg.Thread_VG(url)
+# thread.extract_sets()
+# thread.solve_sets()
+
+# downloader = dw.Downloader('./sets/original', 'Jni - White', thread.sets, img_sleep_secs=0.2)
+# downloader.download()
+
+# url = 'https://simpcity.su/threads/jessica-nigri.9946/'
+# print('hello')
+# url = 'https://simpcity.su/threads/jinx-asmr.18872/'
+# thread = sc.Thread_SC(url, 'Jinx', sc_cookie)
+# thread.extract_sets()
+# thread.solve_sets()
+# print(thread.sets)
+
+# downloader = dw.Downloader('./sets/original', 'Jinx', thread.sets, img_sleep_secs=0.1)
+# downloader.download()
 
